a60c875a1c8bd11b7379acf9ddbc72efac537626
Nicolas Mesa <nicomr2905@gmail.com>
2025-06-01 08:48:41 -0500

    Creating Project

---
+++ src/test/java/com/quipux/test/ReproductionListsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quipux.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReproductionListsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
